473b27c6f8bdcee97128e5f4aed3d1aa049af584
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-22 15:34:53 -0500

    Initial commit. Using Xunit. Using TDD.

---
+++ TddKatas.Tests/BowlingKata/BowlingKataUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace TddKatas.Tests
+{
+    public class BowlingKataUnitTests
+    {
+        [Fact]
+        public void DoesGameExist()
+        {
+            Game g = new Game();
+
+            Assert.NotNull(g);
+        }
+
+        [Fact]
+        public void GutterGameReturns0()
+        {
+            Game g = new Game();
+
+            int pins = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.roll(pins);
+            }
+
+            Assert.Equal(0, g.scoreGame());
+
+
+        }
+    }
+}
---
+++ TddKatas/BowlingKata/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddKatas
+{
+    public class Game
+    {
+        public void roll(int pins)
+        {
+            
+        }
+
+        public int scoreGame()
+        {
+            return 0;
+        }
+    }
+}
